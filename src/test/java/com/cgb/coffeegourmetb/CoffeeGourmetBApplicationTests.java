aafb869e0a41a051e82d5573552d0867d2bf03ab
HolmanGonzalez19 <holmangonzalez59@gmail.com>
2026-07-13 23:27:38 -0500

    Initial Spring Boot backend setup

---
+++ src/test/java/com/cgb/coffeegourmetb/CoffeeGourmetBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgb.coffeegourmetb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeGourmetBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
